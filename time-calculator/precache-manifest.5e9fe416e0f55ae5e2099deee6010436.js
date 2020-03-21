6de97f742be42fa130b7f76aef84ce67ac5272bd
Bjoern Ebbrecht <b.ebbrecht@rl-3.de>
2020-03-21 20:19:59 +0100

    added first version of the time adder

---
+++ time-calculator/precache-manifest.5e9fe416e0f55ae5e2099deee6010436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe8fb4efde2c8a85609662c81df18ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ace6c069c74826ab3bf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "39b6bea0a1fd4b61249c",
+    "url": "/static/js/2.b768c27a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b768c27a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ace6c069c74826ab3bf",
+    "url": "/static/js/main.f11e1b09.chunk.js"
+  },
+  {
+    "revision": "7e0c4c15294e32de34d8",
+    "url": "/static/js/runtime-main.203003f7.js"
+  }
+]);
